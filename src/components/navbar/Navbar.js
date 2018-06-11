69d89fe36fe65ca187ead10fe7e800d64a28fc15
Mohammed Hameed <mohammed.hameed@email.com>
2018-06-11 02:11:56 -0500

    some color styling, no biggie

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,10 +9,10 @@ class Navbar extends Component {
                     Smurf Memory Game
                 </div>
                 <div className = "float-right navbarTextSmall mt-3">
-                    <div className = "d-inline mr-5">Score:<span className = "textColor">{this.props.score}</span></div>
-                    <div className = "d-inline mr-5">High Score:<span  className = "textColor">{this.props.highScore}</span></div>
-                    <div className = "d-inline mr-5">Win:<span  className = "textColor">{this.props.win}</span></div>
-                    <div className = "d-inline mr-5">Loss:<span  className = "textColor">{this.props.loss}</span></div>
+                    <div className = "d-inline mr-5">Score: <span className = "text-warning">{this.props.score}/12</span></div>
+                    <div className = "d-inline mr-5">High Score: <span  className = "text-warning">{this.props.highScore}</span></div>
+                    <div className = "d-inline mr-5">Win: <span  className = "text-warning">{this.props.win}</span></div>
+                    <div className = "d-inline mr-5">Loss: <span  className = "text-warning">{this.props.loss}</span></div>
 
                 </div>
             </div>
